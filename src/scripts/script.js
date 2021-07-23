b3ae45ae4edd74d6a5acf2d569ffcfabc245dc37
Vladislav Flicker <flickervladislav@gmail.com>
2021-07-23 09:44:18 +0300

    Add site-header. WIP

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,9 @@
-'use strict';
+var navigation = document.querySelector('.main-nav');
+var toggleButton = document.querySelector('.main-nav__toggle');
+
+var toggleButtonClickHandler = function(evt) {
+  evt.preventDefault();
+  navigation.classList.toggle('main-nav--open');
+};
+
+toggleButton.addEventListener('click', toggleButtonClickHandler);
